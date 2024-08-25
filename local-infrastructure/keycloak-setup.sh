2d4d8fb1a12b3c1b258dfde86023e73c18d82d1f
Christian Zyweck <c.zyweck@gmail.com>
2024-08-25 08:12:55 +0200

    feat: adds creation of keycloak realm and users to local infrastructure setup

---
+++ local-infrastructure/keycloak-setup.sh
@@ -0,0 +1,43 @@
+#!bin/bash
+
+REALM_NAME=mealplanner
+
+# warten, bis der keycloak hochgefahren ist
+while ! echo > /dev/tcp/localhost/8080; do
+  sleep 5
+done
+
+# login des kc admin
+/opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password admin
+
+REALM_EXISTS() {
+ /opt/keycloak/bin/kcadm.sh get realms -q realm=$REALM_NAME | grep -q '"realm"'
+}
+
+USER_EXISTS() {
+  /opt/keycloak/bin/kcadm.sh get users -r $REALM_NAME -q username=$1 | grep -q '"id"'
+}
+
+# array von benutzern, die erstellt werden sollen
+USERS=(
+  "mpadmin"
+  "mpuser1"
+  "mpuser2"
+)
+
+# realm erstellen, wenn er noch nicht existiert
+if ! REALM_EXISTS; then
+  /opt/keycloak/bin/kcadm.sh create realms -s realm=$REALM_NAME -s enabled=true
+else
+  echo "Realm '$REALM_NAME' already exists"
+fi
+
+# user erstellen, wenn sie noch nicht existieren
+for USERNAME in "${USERS[@]}"; do
+  if USER_EXISTS $USERNAME; then
+    echo "User '$USERNAME' already exists"
+  else
+    /opt/keycloak/bin/kcadm.sh create users -r $REALM_NAME -s username=$USERNAME -s enabled=true -s email="$USERNAME@example.com" -s emailVerified=true -s credentials='[{"type":"password","value":"'$USERNAME'","temporary":false}]'
+  fi
+done
+

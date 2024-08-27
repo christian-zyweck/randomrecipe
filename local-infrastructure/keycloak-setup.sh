26d4543a8d2f849658408cdb8a963ecba27e9c83
Christian Zyweck <c.zyweck@gmail.com>
2024-08-27 05:47:05 +0200

    feat(#6): adds creation of a client in the keycloak setup, tidies up the script a bit

--- local-infrastructure/keycloak-setup.sh
+++ local-infrastructure/keycloak-setup.sh
@@ -1,8 +1,29 @@
 #!bin/bash
 
+# realm and client names
 REALM_NAME=mealplanner
+CLIENT_NAME=api-user
 
-# warten, bis der keycloak hochgefahren ist
+# array of users to create
+USERS=(
+  "mpadmin"
+  "mpuser1"
+  "mpuser2"
+)
+# functions that check the existence of resources in keycloak
+REALM_EXISTS() {
+  /opt/keycloak/bin/kcadm.sh get realms/$REALM_NAME | grep -q "$REALM_NAME"
+}
+
+CLIENT_EXISTS() {
+  /opt/keycloak/bin/kcadm.sh get clients -r mealplanner -q clientId=$CLIENT_NAME | grep -q '"id"'
+}
+
+USER_EXISTS() {
+  /opt/keycloak/bin/kcadm.sh get users -r $REALM_NAME -q username=$1 | grep -q '"id"'
+}
+
+# wait until keycloak is up and running
 while ! echo > /dev/tcp/localhost/8080; do
   sleep 5
 done
@@ -10,32 +31,28 @@ done
 # login des kc admin
 /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password admin
 
-REALM_EXISTS=$( /opt/keycloak/bin/kcadm.sh get realms/$REALM_NAME | grep -q "$REALM_NAME" && echo "true" || echo "false" )
-
-USER_EXISTS() {
-  /opt/keycloak/bin/kcadm.sh get users -r $REALM_NAME -q username=$1 | grep -q '"id"'
-}
-
-# array von benutzern, die erstellt werden sollen
-USERS=(
-  "mpadmin"
-  "mpuser1"
-  "mpuser2"
-)
-
-# realm erstellen, wenn er noch nicht existiert
-if [ "$REALM_EXISTS" = "true" ]; then
-  echo "Realm '$REALM_NAME' exists."
+# create realm if non existent
+if REALM_EXISTS; then
+  echo "Realm '$REALM_NAME' already exists. Skipping creation."
 else
+  echo "Realm '$REALM_NAME' does not exist. Creating realm."
   /opt/keycloak/bin/kcadm.sh create realms -s realm=$REALM_NAME -s enabled=true
 fi
 
-# user erstellen, wenn sie noch nicht existieren
+# create client if non existent
+if CLIENT_EXISTS; then
+  echo "Client '$CLIENT_NAME' already exists. Skipping creation"
+else
+  echo "Client '$CLIENT_NAME' does not exist. Creating client."
+    /opt/keycloak/bin/kcadm.sh create clients -r $REALM_NAME -s clientId=$CLIENT_NAME -s enabled=true -s directAccessGrantsEnabled=true -s publicClient=true
+fi
+
+# create users if non existent
 for USERNAME in "${USERS[@]}"; do
   if USER_EXISTS $USERNAME; then
-    echo "User '$USERNAME' already exists"
+    echo "User '$USERNAME' already exists. Skipping creation."
   else
-    /opt/keycloak/bin/kcadm.sh create users -r $REALM_NAME -s username=$USERNAME -s enabled=true -s email="$USERNAME@example.com" -s emailVerified=true -s credentials='[{"type":"password","value":"'$USERNAME'","temporary":false}]'
+    echo "User '$USERNAME' does not exist. Creating user."
+    /opt/keycloak/bin/kcadm.sh create users -r $REALM_NAME -s username=$USERNAME -s enabled=true -s email="$USERNAME@example.com" -s emailVerified=true -s credentials='[{"type":"password","value":"'$USERNAME'","temporary":false}]' -s firstName="$USERNAME" -s lastName="$USERNAME"
   fi
 done
-

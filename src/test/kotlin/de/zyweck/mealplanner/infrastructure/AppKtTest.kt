3bb10e3dfe2aa02b81b1b992bad5df68f1dd43e7
Christian Zyweck <c.zyweck@gmail.com>
2024-08-27 05:51:37 +0200

    test(#6): adds keycloak testcontainer with realm file to import, adds test cases for checking if the keycloak integration is working as expected

--- src/test/kotlin/de/zyweck/mealplanner/infrastructure/AppKtTest.kt
+++ src/test/kotlin/de/zyweck/mealplanner/infrastructure/AppKtTest.kt
@@ -1,8 +1,11 @@
 package de.zyweck.mealplanner.infrastructure
 
+import dasniko.testcontainers.keycloak.KeycloakContainer
 import io.restassured.RestAssured
+import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
+import io.restassured.path.json.JsonPath
 import org.hamcrest.Matchers.equalToIgnoringCase
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -20,11 +23,19 @@ import org.testcontainers.junit.jupiter.Testcontainers
 class AppKtTest(
     @LocalServerPort private val localPort: Int,
 ) {
+    private val clientId = "api-user"
+
     companion object {
         @Container
         val postgres =
             PostgreSQLContainer("postgres:16-alpine").apply {
                 withDatabaseName("mealplanner-junit")
+            }
+
+        @Container
+        val keycloakContainer =
+            KeycloakContainer().apply {
+                withRealmImportFile("/keycloak/mealplanner-realm.json")
             }
 
         @JvmStatic
@@ -33,6 +44,7 @@ class AppKtTest(
             registry.add("spring.datasource.url", postgres::getJdbcUrl)
             registry.add("spring.datasource.username", postgres::getUsername)
             registry.add("spring.datasource.password", postgres::getPassword)
+            registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri") { keycloakContainer.authServerUrl + "/realms/mealplanner" }
         }
     }
 
@@ -49,5 +61,47 @@ class AppKtTest(
             statusCode(200)
             body("status", equalToIgnoringCase("up"))
         }
+    }
+
+    @Test
+    fun `unauthenticated request to a non-existent controller should result in HTTP-403`() {
+        When {
+            get("/i/dont/exist")
+        } Then {
+            statusCode(401)
+        }
+    }
+
+    @Test
+    fun `authorized request to a non-existent controller should result in http-404`() {
+        val token = getToken(clientId, "mpadmin", "mpadmin")
+        Given {
+            auth().oauth2(token)
+        } When {
+            get("/i/dont/exist")
+        } Then {
+            statusCode(404)
+        }
+    }
+
+    private fun getToken(
+        clientId: String,
+        userName: String,
+        password: String,
+    ): String {
+        val response =
+            Given {
+                contentType("application/x-www-form-urlencoded")
+                formParam("grant_type", "password")
+                formParam("client_id", clientId)
+                formParam("username", userName)
+                formParam("password", password)
+            } When {
+                post("${keycloakContainer.authServerUrl}/realms/mealplanner/protocol/openid-connect/token")
+            }
+
+        val responseBody = response.body.asString()
+
+        return JsonPath(responseBody).getString("access_token")
     }
 }
